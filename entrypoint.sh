d1c13c66ff8f9992e746a87bc4497af6e3828e4b
Frederic Leger <frederic@webofmars.com>
2023-10-13 22:26:36 +0200

    fix: dockerfile errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ set -o pipefail
 [ -z "${HEADER}" ] && echo "HEADER is required" && exit 1
 [ -z "${VALUE}" ] && echo "VALUE is required" && exit 1
 
-/app/http-header-authenticator -H "${HEADER}" -v "${VALUE}"
+/usr/local/bin/http-header-authenticator check -H "${HEADER}" -V "${VALUE}"
